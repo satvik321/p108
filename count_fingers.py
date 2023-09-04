200b0b992360c7716b2e6527d966a295fe5c7fdf
satvik321 <99606451+satvik321@users.noreply.github.com>
2023-09-04 15:54:28 -0400

    Add files via upload

---
+++ count_fingers.py
@@ -0,0 +1,79 @@
+import cv2
+import mediapipe as mp
+
+cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
+
+tipIds = [4, 8, 12, 16, 20]
+
+# Define a function to count fingers
+def countFingers(image, hand_landmarks, handNo=0):
+    print()
+           
+    if hand_landmarks:
+       # Get All The Landmarks Of the First Hand Visible
+       landMarks = hand_landmarks[handNo].landmark
+       fingers=[]
+       for lm_index in tipIds:
+          # Get Finger Tip And Bottom Id Position
+          fingerTipY=landMarks[lm_index].y
+          fingerBottomY=landMarks[lm_index - 2].y
+          thumbTipX=landMarks[lm_index].x
+          thumbBottomX=landMarks[lm_index].x
+
+          # Check If any Finger Is Open Or Closed
+          if lm_index != 4:
+             
+           
+             if thumbTipX > thumbBottomX:
+                fingers.append(1)
+                print("Like")
+             if thumbTipX < thumbBottomX:
+                fingers.append(0)
+                print("Dislike")
+       totalFingers = fingers.count(1)
+       text= f"Fingers : {totalFingers}"
+       cv2.putText(image,text,(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
+
+# Define a function to 
+def drawHandLanmarks(image, hand_landmarks):
+
+    # Darw connections between landmark points
+    if hand_landmarks:
+
+      for landmarks in hand_landmarks:
+               
+        mp_drawing.draw_landmarks(image, landmarks, mp_hands.HAND_CONNECTIONS)
+
+
+while True:
+    success, image = cap.read()
+
+    image = cv2.flip(image, 1)
+    
+    # Detect the Hands Landmarks 
+    results = hands.process(image)
+ 
+    # Get landmark position from the processed result
+    hand_landmarks = results.multi_hand_landmarks
+
+    # Draw Landmarks
+    drawHandLanmarks(image, hand_landmarks)
+
+    # Get Hand Fingers Position 
+    countFingers(image,hand_landmarks)       
+
+
+
+    cv2.imshow("Media Controller", image)
+
+    # Quit the window on pressing Sapcebar key
+    key = cv2.waitKey(1)
+    if key == 32:
+        break
+
+cv2.destroyAllWindows()
